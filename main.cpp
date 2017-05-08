40cdf3c205ebb634d3f878c0e7f478f305d17f78
GameMachine32 <legomail90520@gmail.com>
2017-05-08 10:13:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <string>
+#include "base.h"
+using namespace std;
+
+int main()
+{
+ system("COLOR 0A");
+ cout<<" ### #     # ####### #######  #####  ####### ####### ######  "  << endl;
+  cout<<"  #  ##    # #       #       #     #    #    #       #     # " << endl;
+  cout<<"  #  # #   # #       #       #          #    #       #     # " << endl;
+  cout<<"  #  #  #  # #####   #####   #          #    #####   #     # " << endl;
+  cout<<"  #  #   # # #       #       #          #    #       #     # " << endl;
+  cout<<"  #  #    ## #       #       #     #    #    #       #     # "  << endl;
+ cout<<" ### #     # #       #######  #####     #    ####### ######  " << endl;
+ system("pause");
+ base();
+
+
+}
